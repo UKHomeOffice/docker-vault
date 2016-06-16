50c9237bd370c1936f68936d2c82b8b2af17a1d6
Vaidas Jablonskis <jablonskis@gmail.com>
2016-06-16 16:13:36 +0100

    Rewrite

---
+++ README.md
@@ -0,0 +1,64 @@
+# Vault in Kubernetes
+
+Vault in a docker image with all the necessary scripts to run vault in
+kubernetes cluster.
+
+There are two main components in this setup:
+- Vault container - main vault process which listens in a tcp socket
+- overlord container - bash script which takes care of unsealing vault,
+  creating admin user and persisting vault unseal key
+
+
+## Getting Started
+
+First of all, you need to make sure that your kubernetes cluster supports
+service accounts and that either the default or vault specific service account
+has access to create kubernetes secrets. However this is only needed if you're
+bootstrapping vault.
+
+We are going assume that vault is being deployed into a namespace called vault.
+
+In the below example we will use AWS DynamoDB as a backend. So for that, you
+need to create a DynamoDB table and an IAM user with required permissions. Then
+change environment variables accordingly in [vault deployment file](kube/vault-deployment.yaml).
+
+
+### Configuration
+
+* `VAULT_BACKEND` - defaults to `inmem`.
+* `TLS_DISABLE` - defaults to 1.
+* `VAULT_ADMIN_PASSWORD` - admin password that overlord sets when creating an
+  admin user. If unset, overlord will generate a random one, which will be
+  logged, so changing it is advisable.
+
+Any other environment variables, which are supported by vault, can be set.
+
+
+### Deployment
+
+* Deploy an empty vault-unseal secret (will be updated by overlord script)
+`kubectl --namespace=vault create -f kube/vault-secrets.yaml`
+
+* Deploy vault pod (vault itself and overlord container)
+`kubectl --namespace=vault create -f kube/vault-deployment.yaml`
+
+* Deploy a kubernetes service endpoint for vault
+`kubectl --namespace=vault create -f kube/vault-svc.yaml`
+
+
+### Other
+
+#### TLS
+
+If you want to provide TLS certs, you can place them in `/vault/certs/cert.pem`
+and `/vault/certs/key.pem`.
+
+
+## Contributing
+
+Contributions are most certainly welcome. If you want to introduce a breaking
+change or any other major change, please raise an issue first to discuss.
+
+## License
+
+[MIT](LICENSE)
---
+++ bin/overlord.sh
@@ -0,0 +1,138 @@
+#!/usr/bin/bash
+
+[[ ${DEBUG} == 'true' ]] && set -x
+
+set -o errexit
+
+source /vault/bin/env.sh
+VAULT_UNSEAL_FILE='/vault/secrets/vault-unseal.key'
+
+
+function failed() {
+  (>&2 echo "[error] $@") && exit 1
+}
+
+
+function announce() {
+  (>&2 echo "[info] $@")
+}
+
+
+function retry() {
+  local counter=0
+  until [[ ${counter} -ge 3 ]]
+  do
+    ${1} && break
+    counter=$[${counter}+1]
+    announce 'retrying again in 3 seconds.'
+    sleep 1
+  done
+}
+
+
+function vault_alive() {
+  if ! curl -s ${VAULT_ADDR} > /dev/null; then
+    announce "vault is not ready."
+    return 1
+  fi
+  return 0
+}
+
+
+function vault_initialized() {
+  vault init -check > /dev/null && return 0 || return 1
+}
+
+
+function vault_sealed() {
+  vault status 2>&1 | grep -q 'Sealed: true' && return 0 || return 1
+}
+
+
+function create_kubernetes_secret() {
+  announce 'writing unseal key to kubernetes secret.'
+
+  cat <<EOF | kubectl --namespace=${KUBERNETES_NAMESPACE} replace -f - > /dev/null
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: vault-unseal
+data:
+  vault-unseal.key: $(echo -n ${VAULT_UNSEAL_KEY} | base64 -w 0)
+EOF
+
+  if [[ ${?} -ne 0 ]]; then
+    failed 'failed to write unseal key as kubernetes secret. You will need to start over.'
+  else
+    announce 'unseal key has been recorded as kubernetes secret.'
+  fi
+}
+
+
+function initialize_vault() {
+  announce 'initializing vault.'
+  _tmpfile=$(mktemp /tmp/vault.XXXX)
+  vault init -key-shares=1 -key-threshold=1 > ${_tmpfile}
+  vault_initialized || failed 'failed to initialize vault.'
+
+  VAULT_UNSEAL_KEY=$(awk -F': ' '/^Unseal Key 1/{e=0; print $2; exit} {e=1} END{exit e}' ${_tmpfile} || failed 'failed to read unseal key.')
+  VAULT_ROOT_KEY=$(awk -F': ' '/^Initial Root Token/{e=0; print $2; exit} {e=1} END{exit e}' ${_tmpfile} || failed 'failed to read root key.')
+
+  export VAULT_TOKEN=${VAULT_ROOT_KEY}
+  announce 'vault has been initialized.'
+}
+
+
+function unseal_vault() {
+  if [[ ! -n ${VAULT_UNSEAL_KEY} ]]; then
+    [[ -f ${VAULT_UNSEAL_FILE} ]] || failed 'unable to find unseal key. Please specify vault unseal key.'
+    VAULT_UNSEAL_KEY=$(cat ${VAULT_UNSEAL_FILE})
+  fi
+
+  announce 'unsealing vault.'
+  if vault unseal ${VAULT_UNSEAL_KEY} 1> /dev/null; then
+    announce 'vault has been unsealed.'
+  else
+    failed 'failed to unseal vault'
+  fi
+}
+
+
+function create_admin_user() {
+  announce 'creating an admin user.'
+  local _p
+  if [[ ! -n ${VAULT_ADMIN_PASSWORD} ]]; then
+    _p=$(head -c18 < /dev/urandom | base64 | tr -d '\n')
+    announce "no admin password specified. Generating random one (please change it): ${_p}"
+  else
+    _p=${VAULT_ADMIN_PASSWORD}
+  fi
+
+  if vault auth-enable userpass > /dev/null; then
+    announce 'successfully enabled userpass auth backend.'
+    vault write auth/userpass/users/admin password=${_p} policies=root || (announce 'unable to create admin user.'; return 1)
+    return 0
+  else
+    announce 'unable to enable userpass auth backend.'
+    return 1
+  fi
+}
+
+
+while true; do
+  if vault_alive; then
+    if vault_initialized; then
+      vault_sealed && unseal_vault
+    else
+      initialize_vault
+      vault_sealed && unseal_vault
+      retry create_admin_user
+      create_kubernetes_secret
+    fi
+    sleep 30
+  else
+    announce 'sleeping for 10s.'
+    sleep 10
+  fi
+done
