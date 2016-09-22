5054fb91e3e0e7ec4228c98a62bd0b14920abaac
Vaidas Jablonskis <jablonskis@gmail.com>
2016-09-22 21:29:44 +0100

    Set advertise_addr
    Even if HA is not being used, say, in dynamodb backend, vault insists on
    advertise_addr being set.

--- bin/env.sh
+++ bin/env.sh
@@ -15,6 +15,12 @@
 
 if [[ ${TLS_DISABLE} == '0' ]]; then
   export VAULT_ADDR='https://127.0.0.1:8200'
+  if [[ -n ${VAULT_ADVERTISE_IP} ]]; then
+    export VAULT_ADVERTISE_ADDR=https://${VAULT_ADVERTISE_IP}:8200
+  fi
 else
   export VAULT_ADDR='http://127.0.0.1:8200'
+  if [[ -n ${VAULT_ADVERTISE_IP} ]]; then
+    export VAULT_ADVERTISE_ADDR=http://${VAULT_ADVERTISE_IP}:8200
+  fi
 fi
