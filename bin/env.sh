db42c1c7571d57cf076b33fa84aa5d03ac3e0c58
Vaidas Jablonskis <jablonskis@gmail.com>
2016-09-22 21:28:01 +0100

    Set KUBERNETES_NAMESPACE explicitly instead

--- bin/env.sh
+++ bin/env.sh
@@ -3,7 +3,6 @@
 [[ ${DEBUG} == 'true' ]] && set -x
 
 : ${OVERLORD_DAEMON:=true}
-: ${KUBERNETES_NAMESPACE:=$(cat /run/secrets/kubernetes.io/serviceaccount/namespace)}
 : ${VAULT_CONFIG:=/vault/vault.hcl}
 : ${VAULT_BACKEND:=inmem}
 : ${VAULT_SKIP_VERIFY:=false}
